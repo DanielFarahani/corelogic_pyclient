f22bfdb8f947bc9dd3ef0ba8f60e3a63801472ca
danielfarahani <danfarahani@gmail.com>
2020-07-25 21:11:07 +1000

    testing census endpoints

--- app/census.py
+++ app/census.py
@@ -10,21 +10,40 @@ class Census(Authentication):
         super().__init__(False)
         self.url += "/statistics"
 
+
+    #TODO just push this to census as kwargs
     def summary(self, location_id=12606, location_type_id=8):
-        #TODO just push this to census as kwargs
+        r'''
+        returns:
+        {
+            "censusSummaryDescription": "string",
+            "councilAreaName": "string",
+            "countryName": "string",
+            "errors": [ { "msg": "string" } ],
+            "fiveYearPopulationChange": "string",
+            "localityName": "string",
+            "locationType": "string",
+            "postcodeName": "string",
+            "stateName": "string",
+            "territorialAuthorityName": "string"
+        }
+        '''
+        
         params = {
             'locationId': location_id,
             'locationTypeid': location_type_id
         }
+
         try:
-            res = requests.get(self.url + "/census/summary", params=params, headers=self.headers).json().__dict__()
-            
+            res = requests.get(self.url + "/census/summary", params=params, headers=self.headers)
+            res = res.json()
         except HTTPError as err:
-            print(err)
+            print(err) 
+        
+        return res
 
 
 # size, parks (#, %), age group, household, repayment (range/m), occupation type, median salary
-
     def census(
         self, 
         from_date="", 
@@ -34,6 +53,34 @@ class Census(Authentication):
         metric_type_group_id=120, 
         metric_type_id=0
         ):
+        r'''
+        {
+            "censusResponseList": [
+                {
+                "councilAreaName": "string",
+                "countryName": "string",
+                "isSumStatistic": true,
+                "localityName": "string",
+                "locationType": "string",
+                "metricDisplayType": "string",
+                "metricType": "string",
+                "metricTypeDescription": "string",
+                "metricTypeGroup": "string",
+                "metricTypeGroupDescription": "string",
+                "metricTypeGroupId": 0,
+                "metricTypeId": 0,
+                "metricTypeOrderId": 0,
+                "metricTypeShort": "string",
+                "postcodeName": "string",
+                "propertyType": "string",
+                "seriesDataList": [ { "dateTime": "2020-07-25T11:07:20.896Z", "value": 0 } ],
+                "stateName": "string",
+                "territorialAuthorityName": "string"
+                }
+            ],
+            "errors": [ { "msg": "string" } ]
+        }
+        '''
 
         payload = {
             'censusRequestList': [
@@ -53,3 +100,7 @@ class Census(Authentication):
             
         except HTTPError as err:
             print(err)
+
+if __name__ == "__main__":
+    c = Census()
+    print(c.summary())
