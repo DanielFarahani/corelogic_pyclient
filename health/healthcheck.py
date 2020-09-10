91b38b1e8e9cc5039e0cadcd05ebbfa307bdde59
danielfarahani <danfarahani@gmail.com>
2020-09-10 12:17:44 +1000

    added boolean in return value

--- health/healthcheck.py
+++ health/healthcheck.py
@@ -1,7 +1,5 @@
 import requests
 
-
-
 class healthcheck:
 
     def __init__(self):
@@ -9,11 +7,12 @@ class healthcheck:
         self.description = { "GREEN": "available and normal.", "BLUE": "going through the release process.", 
         "AMBER": "partially stable." ,"RED": "not available."}
 
-
     def healthcheck_statistics(self):
         r'''
             Statistics endpoint health check
-            returns: status and description
+            returns: 
+            State: boolean for logic flow 
+            Info: for status and description
         '''
         endpoint = "/statistics/env/health"
         try:
@@ -21,11 +20,12 @@ class healthcheck:
             headers= {}
             response = requests.get(self.url + endpoint, headers=headers, data = payload)
             response = response.json()
-            response['description'] = self.description[response['status']]
+            response['description'] = self.description[response['status']
+            state = True if response['status'] == 'GREEN' else False
         except Exception as error:
             return error
 
-        return response
+        return state, response
 
 
 if __name__ == "__main__":
