304e1e9d9d6335ff1341bac167c811daacef3b2d
danielfarahani <danfarahani@gmail.com>
2020-12-04 17:06:06 +1100

    setup new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="corelogic_pyclient",
-      version="0.1.2",
+      version="0.1.3",
       description="CoreLogic API Wrapper for interacting with CoreLogic (Property Data Reports & Analytics)",
       url="https://github.com/danielfarahani/corelogic_pyclient",
       classifiers=[
